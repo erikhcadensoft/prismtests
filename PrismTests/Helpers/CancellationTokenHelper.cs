400ed9626960a1799d3431e85c648509f2b2f6c1
Erik Hornyak <erikhornyak@eriks-mbp-2019.lan>
2020-01-05 19:10:39 -0500

    add resources and helpers to main solution

---
+++ PrismTests/Helpers/CancellationTokenHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+
+namespace PrismTests.Helpers
+{
+    public static class CancellationTokenHelper
+    {
+        /// <summary>
+        /// Setups the cancellation token.
+        /// </summary>
+        /// <param name="cancellationTokenSource">The cancellation token source.</param>
+        /// <param name="timeoutInMilliseconds">The timeout in milliseconds value.</param>
+        public static CancellationTokenSource SetupCancellationToken(CancellationTokenSource cancellationTokenSource, int timeoutInMilliseconds = 0)
+        {
+            try
+            {
+                // Try to cancel any previous task
+                cancellationTokenSource?.Cancel();
+            }
+            catch (ObjectDisposedException)
+            {
+                // Soak it up...
+            }
+
+            // Ensure we have a valid CancellationTokenSource, if not, create a new instance
+            cancellationTokenSource = new CancellationTokenSource();
+
+            // If we provided a valid timeout in seconds value set our CancellationTokenSource with it, in milliseconds.
+            if (timeoutInMilliseconds > 0) cancellationTokenSource.CancelAfter(timeoutInMilliseconds);
+
+            // Return CancellationTokenSource
+            return cancellationTokenSource;
+        }
+
+        /// <summary>
+        /// Cancels and disposes the cancellation token source object.
+        /// </summary>
+        /// <param name="cancellationTokenSource">The cancellation token source.</param>
+        /// <param name="createNewCancellationTokenSource">The create new cancellation token source.</param>
+        /// <returns>Task&lt;System.Boolean&gt;.</returns>
+        public static bool CleanupCancellationToken(ref CancellationTokenSource cancellationTokenSource, bool createNewCancellationTokenSource = false)
+        {
+            try
+            {
+                // Validate
+                if (cancellationTokenSource != null)
+                {
+                    // Cleanup
+                    cancellationTokenSource?.Cancel();
+                    cancellationTokenSource?.Dispose();
+
+                    // Do we want to create a new CancellationTokenSource?
+                    if (createNewCancellationTokenSource)
+                    {
+                        // Create new instance of CancellationTokenSource
+                        cancellationTokenSource = new CancellationTokenSource();
+                    }
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                // Default
+                return false;
+            }
+
+            // Default
+            return true;
+        }
+    }
+}
---
+++ PrismTests/Helpers/Logger.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using Microsoft.AppCenter.Analytics;
+//using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+
+namespace PrismTests.Helpers
+{
+    public static class Logger
+    {
+        public static void TrackLoadTime(string eventName, TimeSpan? ts, Dictionary<string, string> additionalProperties = null)
+        {
+            try
+            {
+                string elapsedTime = $" | Time to Load: Hours: {ts?.Hours:00} Minutes:{ts?.Minutes:00} Seconds:{ts?.Seconds:00}.{ts?.Milliseconds / 10:00}";
+                var key = additionalProperties.ElementAt(0).Key;
+                additionalProperties[key] = string.Concat(additionalProperties[key], elapsedTime);
+                LogEvent(eventName, "TrackLoadTime", additionalProperties);
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+        }
+
+        public static void LogEvent(string eventName, string message, Dictionary<string, string> additionalProperties = null)
+        {
+            var properties = new Dictionary<string, string>
+            {
+                { "Device Type", Device.RuntimePlatform },
+                { "Message", message }
+            };
+
+            if (additionalProperties != null)
+            {
+                var i = 0;
+                foreach (var prop in additionalProperties)
+                {
+                    var key = additionalProperties.ElementAt(i).Key;
+                    var val = additionalProperties.ElementAt(i).Value;
+                    properties.Add(key, val);
+                    i++;
+                }
+            }
+
+            //Analytics.TrackEvent(eventName, properties);
+        }
+
+        public static void LogError(Exception exception)
+        {
+            var properties = new Dictionary<string, string>
+            {
+                { "Device Type", Device.RuntimePlatform },
+                { "Message", exception.Message }
+            };
+
+            //Crashes.TrackError(exception, properties);
+        }
+    }
+}
